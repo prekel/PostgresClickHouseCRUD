fa300cc991c5178f890997c0df2686ed413cce5d
Vladislav Prekel <misterptits@yandex.ru>
2020-08-25 16:17:09 +0700

    some csproj

---
+++ PostgresClickHouseCRUD.App1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using PostgresClickHouseCRUD.ClickHouse;
+
+namespace PostgresClickHouseCRUD.App1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var q = new Class1();
+
+            q.Test1();
+        }
+    }
+}
